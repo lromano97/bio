2c211fe9a2db065286e9888bc3d160d1fdc54a93
lromano97 <romano.97lucas@gmail.com>
2019-10-06 19:37:39 -0300

    Ejercicio 1 + instrucciones

---
+++ Ejercicio 1/Ex1.pm
@@ -0,0 +1,23 @@
+use Bio::SeqIO;
+use Bio::PrimarySeq;
+
+$file_name = 'sequence.gb';
+
+# Leo del archivo sequence.gb la secuencia del gen
+$seqio_obj = Bio::SeqIO->new(-file=> $file_name, -format=> 'genbank');
+
+$prot_obj = Bio::SeqIO->new(-file => '>protein.fas', -format=>'fasta');
+
+#Itero sobre cada secuencia que se encuentra en el archivo recibido como input para poder llevar a cabo la traduccion a cadenas de aminoacidos
+while ($ind_seq = $seqio_obj->next_seq) {
+    # Chequeo que la secuencia a traducir sea correcta, de no ser así se aborta el loop, terminando el proceso.
+    if($ind_seq->validate_seq($ind_seq->seq)) {
+        # Traduzco la secuencia en base a los 6 marcos de lectura posibles con la función translate_6frames, los mismos van a ser almacenados en el array proteins
+        @proteins = Bio::SeqUtils->translate_6frames($ind_seq);
+        # Volcado de la secuencia al archivo de salida
+        $prot_obj->write_seq(@proteins);
+    } else {
+        # En el caso de que el resultado de la validación de que es una secuencia invalida se aborta el procesamiento del mismo.
+        die 'La secuencia leida es invalida';
+    }
+}
---
+++ Ejercicio 1/Instructions.md
@@ -0,0 +1,21 @@
+# Ejercicio 1
+## Requisitos
+- Tener instalado Perl
+- Tener instalado Bio::SeqIO y Bio::PrimarySeq, para eso
+
+```bash 
+    cpanm Bio::SeqIO
+```
+
+```bash 
+    cpanm Bio::PrimarySeq
+```
+
+## Proceso
+- Se descargó el gen de la página https://www.ncbi.nlm.nih.gov a partir del cual se realizara el procesamiento para poder obtener la secuencia de aminoacidos.
+- Dentro del archivo `Ex1.pm` se encontrara la variable `$file_name` que hace referencia al archivo que se tomará para llevar a cabo la ejecución.
+- Una vez modificado el mismo (en el caso de que sea necesario), ejecutar el script con
+```bash
+    perl Ex1.pm
+```
+- Tras su ejecución se generará el archivo `protein.fas` en el cual se encontrarán los 6 posibles marcos de lectura de dicho gen.
